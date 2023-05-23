ecff063c9502dae6addf09bf1920ab8f4884c586
JoshSB-GIT <sierrajoseph8023@gmail.com>
2023-05-23 13:05:48 -0500

    grafica xd

--- src/models/graficarPerdida.py
+++ src/models/graficarPerdida.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 
 
 class GraficarPerdida:
+
+    _path_img = './src/assets/img/'
+
     def __call__(self, perdida):
         epochs = range(1, len(perdida) + 1)
         plt.plot(epochs, perdida, 'b', label='Función de pérdida')
@@ -9,3 +12,5 @@ class GraficarPerdida:
         plt.xlabel('Épocas')
         plt.ylabel('Pérdida')
         plt.legend()
+        file_name = 'graph_neuro'
+        plt.savefig(self._path_img+file_name)
--- src/routes/neuronaRoutes.py
+++ src/routes/neuronaRoutes.py
@@ -1,11 +1,12 @@
 from routes.principalControlador import Controlador
 from routes.modeloControlador import Modelo as CrearModelo
 from routes.responseController import Response_controller
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, make_response, request
 from flask_cors import cross_origin
 import numpy as np
 
 neuro = Blueprint('neuro', __name__)
+_path_img = './src/assets/img/'
 
 
 @cross_origin
@@ -20,9 +21,10 @@ def hypotenuse():
             modelo = CrearModelo()
             response_controller = Response_controller(hick, hick2)
             controlador = Controlador(modelo, response_controller)
-            response = {'data': str(controlador.ejecutar())}
+            response = {'data': str(controlador.ejecutar()),
+                        'img': 'http://127.0.0.1:5000/img/graph_neuro.png'}
 
         except Exception as ex:
-            response = {'data': str(ex)}
+            return make_response({'data': str(ex)}, 400)
 
     return jsonify(response)
